2c55568a37c74d6c251a074f65b28ce235a5efd6
Aahan <taahan0810@gmail.com>
2023-06-18 08:56:44 -0500

    initial commit

---
+++ cora-mlp-test.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd 
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from dgl.data import CoraGraphDataset
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+from cora-mlp-train import MyMLP
+
